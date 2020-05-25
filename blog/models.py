12c46244edd4ae2d80749a4bab1f7990a9b09a49
shin-ueda <shinueda.px@gmail.com>
2020-05-25 21:09:07 +0900

    add Category,Tag

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,38 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    category = models.ForeignKey('Category', verbose_name='Category',
+                                 on_delete=models.CASCADE, null=True)
+    tag = models.ForeignKey('Tag', verbose_name='Tag', on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField(default=now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = now
         self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Title')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Title')
+
+    class Meta:
+        verbose_name = 'Tag'
+        ordering = ['title']
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category, Tag
 
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'blog/post_list.html', {
+                                                'posts': posts,
+                                                'categories': categories,
+                                                'tags': tags})
